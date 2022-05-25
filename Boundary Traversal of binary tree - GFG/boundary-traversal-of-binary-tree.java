b19b3ab60c9277d5afde9341fdcacfe61a73e50d
Joyesh Debnath <75598588+JoyeshDebnath@users.noreply.github.com>
2022-05-26 00:05:57 +0530

    Added solution - LeetHub

---
+++ Boundary Traversal of binary tree - GFG/boundary-traversal-of-binary-tree.java
@@ -0,0 +1,179 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            Solution T = new Solution();
+            
+            ArrayList <Integer> res = T.boundary(root);
+            for (Integer num : res) System.out.print (num + " ");
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+// class Node  
+// { 
+//https://www.geeksforgeeks.org/boundary-traversal-of-binary-tree///     int data; 
+//     Node left, right; 
+   
+//     public Node(int d)  
+//     { 
+//         data = d; 
+//         left = right = null; 
+//     } 
+// }
+
+class Solution
+{
+    ArrayList<Integer> Ans;
+    
+    //print left boundary top -->> bottom 
+    void printLeft(Node root)
+    {
+          if(root==null)
+            return ;
+        
+          if(root.left!=null)
+          {
+              Ans.add(root.data);
+              printLeft(root.left);
+          }
+          else if(root.right!=null)
+          {
+              Ans.add(root.data);
+              printLeft(root.right);
+          }
+    }
+    
+    //print the right boundary bottom -->> top 
+    void printRight(Node root)
+    {
+        if(root==null)
+             return ;
+             
+        if(root.right!=null)
+        {
+            printRight(root.right);
+            Ans.add(root.data);
+        }
+        
+        else if(root.left!=null)
+        {
+            printRight(root.left);
+            Ans.add(root.data);
+        }
+    }
+    
+    //print the leaves 
+    
+    void printLeaves(Node root)
+    {
+             if(root==null)
+                    return ;
+            
+            printLeaves(root.left);
+            
+            if(root.left==null && root.right==null)
+                             Ans.add(root.data);
+            
+            printLeaves(root.right);
+            
+    }
+    
+	ArrayList <Integer> boundary(Node node)
+	{
+	    
+	    Ans=new ArrayList<>();
+	    Ans.add(node.data);
+	    if(node==null)
+	         return Ans;
+	    printLeft(node.left);
+	    printLeaves(node.left);
+	    printLeaves(node.right);
+	    printRight(node.right);
+	    return Ans;
+	}
+}
