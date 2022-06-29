b5096ea4270e914937f01d323b413bced951352c
Joyesh Debnath <75598588+JoyeshDebnath@users.noreply.github.com>
2022-06-29 20:53:15 +0530

    Time: 3 ms (26.67%), Space: 40.3 MB (79.73%) - LeetHub

---
+++ 754-reach-a-number/754-reach-a-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reachNumber(int target) {
+        
+        int step=0;
+        int sum=0;
+        target=Math.abs(target);
+        
+        while(sum<target)
+        {
+            step++;
+            sum+=step;
+        }
+        
+        while((sum-target)%2!=0)
+        {
+            step++;
+            sum+=step;
+        }
+        return step;
+    }
+}
