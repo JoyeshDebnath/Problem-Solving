413aa86c765af2a7d801feee81ccb9209a81104b
Joyesh Debnath <75598588+JoyeshDebnath@users.noreply.github.com>
2022-09-28 22:41:20 +0530

    Time: 16 ms (28.89%), Space: 67.9 MB (17.71%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -12,7 +12,7 @@ class Solution {
             for(int j=0;j<n;j++){
                 if(i==0)//if the 1st Row 
                 {
-                      // System.out.println("Cell Value="+i+ "   "+j);
+               
                     if(matrix[i][j]=='0')
                             dp[i][j]=0;
                     else 
@@ -21,7 +21,7 @@ class Solution {
                 
                 else if(j==0)//if the 1st Column 
                 {
-                      // System.out.println("Cell Value="+i+ "   "+j);
+                      
                     
                       if(matrix[i][j]=='0')
                              dp[i][j]=0;
@@ -34,39 +34,26 @@ class Solution {
                     if(matrix[i][j]=='0')
                             dp[i][j]=0;
                     else{
-                    // System.out.println("Cell Value="+i+ "   "+j);
+                  
                     int topCell=dp[i-1][j];
-                    // System.out.println("Top Cell="+topCell);
+         
                     int diagonalCell=dp[i-1][j-1];
-                     // System.out.println("Diagonal Cell="+diagonalCell);
+                    
                     int leftCell=dp[i][j-1];
-                     // System.out.println("Left Cell="+leftCell);
+                     
                     
                     dp[i][j]=1+Math.min(diagonalCell,Math.min(leftCell,topCell));
                     }
                 }
-                // System.out.println("DP[I][J]="+dp[i][j]);
+              
                 maxmArea=Math.max(maxmArea,dp[i][j]);
             }
         }
-//          System.out.println();
-//         for(char row[]:matrix){
-//             for(char r:row){
-//                 System.out.print(r+"    ");
-//             }
-//             System.out.println();
-//         }
+
         
         
         
-        
-        // System.out.println();
-        // for(int row[]:dp){
-        //     for(int r:row){
-        //         System.out.print(r+"    ");
-        //     }
-        //     System.out.println();
-        // }
+     
      return maxmArea*maxmArea;
         
     }
