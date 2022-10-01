5a3e59cde089dc92a20717d0fa1fc33b523b3993
Joyesh Debnath <75598588+JoyeshDebnath@users.noreply.github.com>
2022-10-01 12:22:13 +0530

    Time: 11 ms (8.10%), Space: 43.1 MB (8.72%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -10,7 +10,8 @@ class Solution {
               
         memo=new Integer[s.length()+5];
         
-        return decodeWays(s.toCharArray(),0);
+        // return decodeWays(s.toCharArray(),0);//memorisation/recursive 
+        return decodeWaysTab(s.toCharArray());//tabulation
     }
     
     boolean isValid(char digits[],int index)
@@ -39,5 +40,33 @@ class Solution {
               pickTwoDigits=decodeWays(digits,idx+2);
         
         return memo[idx]=pickOneDigits+pickTwoDigits;
-    }
+    }//memo/rec 
+    
+    
+    public int decodeWaysTab(char digits[]){
+        
+        int dp[]=new int[digits.length+2];
+        dp[digits.length]=1;
+        for(int i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]=='0'){
+                dp[i]=0;
+                continue;
+            }
+            if(i==digits.length-1){
+                 dp[i]=1;
+                 continue;
+            }
+            
+            int pickOneDigit=dp[i+1];
+            int pickTwoDigits=0;     
+            if(i<=digits.length-2 && isValid(digits,i))
+                   pickTwoDigits=dp[i+2];
+            
+            dp[i]=pickOneDigit+pickTwoDigits;
+        }
+        
+        return dp[0];
+    }//tabulation 
+    
 }
