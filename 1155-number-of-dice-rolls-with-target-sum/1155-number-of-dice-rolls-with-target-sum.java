8b902f6d04cd6b6722d0c1a47d3ad309f68b5c06
Joyesh Debnath <75598588+JoyeshDebnath@users.noreply.github.com>
2022-10-02 10:18:32 +0530

    Time: 38 ms (48.37%), Space: 41.7 MB (83.88%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -3,9 +3,41 @@ class Solution {
     public int numRollsToTarget(int n, int k, int target) {
         memo=new Integer[target+1][n+1];
         
-        return numRollsToTargetRec(target,n,k);
-        
+        // return numRollsToTargetRec(target,n,k);
+        return numRollsToTargetTab(n,k,target);
     }
+    
+    public int numRollsToTargetTab(int n,int k,int target){
+        
+        int dp[][]=new int[target+1][n+1];
+        for(int i=0;i<=target;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+                if(i==0){
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(j==0){
+                    dp[i][j]=0;
+                    continue;
+                }
+                int ways=0;
+              for(int m=1;m<=k;m++){
+                  if((i-m)>=0 && (j-1)>=0){
+                              ways+=dp[i-m][j-1];
+              
+                               ways%=1000000007;
+                  }
+              }
+                dp[i][j]=ways;
+            }
+        }
+        return dp[target][n];
+    }//tabulation 
     
     public int numRollsToTargetRec(int target,int n,int k){
         //if used up all dice and reached the target
