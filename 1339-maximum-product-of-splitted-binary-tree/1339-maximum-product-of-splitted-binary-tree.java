787b54c006b2c8c91c4a4e3600a6bfce64bded87
Joyesh Debnath <75598588+JoyeshDebnath@users.noreply.github.com>
2022-10-12 12:45:50 +0530

    Time: 55 ms (8.27%), Space: 80.3 MB (9.45%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -13,42 +13,12 @@
  *     }
  * }
  */
-// class Solution {
-//     public int maxProduct(TreeNode root) {
-//         HashSet<Long> sums=new HashSet<>();
-//         int total=dfs(root,sums);
-//         long max=0;
-        
-//         for(long sum:sums)
-//                 max=Math.max(max,sum*(total-sum));
-        
-//         return (int)max%1000000007;
-//     }
-    
-    
-//     public int  dfs(TreeNode root,HashSet<Long> sums)
-//     {
-//         if(root==null)
-//                return 0;
-        
-//         root.val+=dfs(root.left,sums);
-//         root.val+=dfs(root.right,sums);
-//         sums.add((long)root.val);
-//         return root.val;
-//     }
-// }
-
 class Solution {
-      int MOD = (int) (1e9) + 7;
-    public int maxProduct(TreeNode root) {
-        Set<Long> sums = new HashSet<>();
-        int total = dfs(root, sums);
-        long max = 0;
-        for (long sum : sums) {
-            max = Math.max(max, sum * (total - sum));
-        }
-        return (int) (max % MOD);
-    }
+   
+    int MOD = (int) (1e9) + 7;
+    
+   
+        
     public int dfs(TreeNode root, Set<Long> sums) {
         if (root == null)
             return 0;
@@ -56,5 +26,14 @@ class Solution {
         root.val += dfs(root.right, sums);
         sums.add((long) (root.val));
         return root.val;
+    }
+    public int maxProduct(TreeNode root) {
+      Set<Long> sums = new HashSet<>();
+        int total = dfs(root, sums);
+        long max = 0;
+        for (long sum : sums) {
+            max = Math.max(max, sum * (total - sum));
+        }
+        return (int) (max % MOD);
     }
 }
